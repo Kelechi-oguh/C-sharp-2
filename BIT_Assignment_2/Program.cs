001cd1908058a044ba759f2f4675b2a3adb1ae8b
Kelechi Oguh <kelechi.oguh505@gmail.com>
2023-11-22 20:55:22 +0100

    Add project files.

---
+++ BIT_Assignment_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// creating an array
+string[] studentNames = new string[10] { "Ahmed", "Bessy", "Chuks", "Sarah", "Mandy", 
+    "Fred", "Kazim", "Terry", "Randy", "Lizzy" };
+
+List<int> studentAges = new List<int>() { 21, 25, 19, 17, 22, 18, 23, 26, 21, 18 };
+
+int targetAge = 18;
+
+bool studentWithTargetAge = studentAges.Contains(targetAge);
+
+if (studentWithTargetAge)
+    Console.WriteLine("There is an {0} year old student", targetAge);
+else
+    Console.WriteLine("No student is {0} years old", targetAge);
